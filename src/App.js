900029e6b8a953bb099f9b12d31ee06010520fe5
Taichi Tomuro <nico13th@gmail.com>
2021-04-17 23:24:26 +0900

    learn components and props

--- src/App.js
+++ src/App.js
@@ -1,19 +1,30 @@
 import React from "react";
 
 const App = () => {
+  const profiles = [
+    { name: "taro", age: 10 },
+    { name: "hanako", age: 5 },
+    { name: "noName" },
+  ];
   return (
     <div>
-      Hi!
-      <Cat />
-      <Cat />
-      <Cat />
-      <Cat />
+      {profiles.map((profile, index) => {
+        const { name, age } = profile;
+        return <User name={name} age={age} key={index} />;
+      })}
     </div>
   );
 };
 
-const Cat = () => {
-  return <div>meow</div>;
+const User = (props) => {
+  const { name, age } = props;
+  return (
+    <div>
+      Hi!, Im {name} and {age} years old!
+    </div>
+  );
 };
+
+User.defaultProps = { age: 1 };
 
 export default App;
